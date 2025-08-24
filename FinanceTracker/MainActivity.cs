90f8ff5d76661d6112d2619263c623eb717beb67
fedyaeva <aleksandra.fedyaeva@gmail.com>
2025-08-24 23:00:48 +0400

    Создание БД при запуске.

--- FinanceTracker/MainActivity.cs
+++ FinanceTracker/MainActivity.cs
@@ -3,9 +3,12 @@ namespace FinanceTracker;
 [Activity(Label = "@string/app_name", MainLauncher = true)]
 public class MainActivity : Activity
 {
+    private AppDB database;
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
+        
+        database = new AppDB();
 
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
