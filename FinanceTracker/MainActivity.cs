91644a1830a5683568c76dc861e9423a68a03d42
fedyaeva <aleksandra.fedyaeva@gmail.com>
2025-08-25 00:26:17 +0400

    Накидала макет главного экрана, поправила манифест для работы с БД. Разделила классы для работы с БД.

--- FinanceTracker/MainActivity.cs
+++ FinanceTracker/MainActivity.cs
@@ -3,12 +3,12 @@ namespace FinanceTracker;
 [Activity(Label = "@string/app_name", MainLauncher = true)]
 public class MainActivity : Activity
 {
-    private AppDB database;
+    private AppDBManager database;
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
         
-        database = new AppDB();
+        database = new AppDBManager();
 
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
