6f85c3d4bd1527303135602878e72ded8dd83b7e
fedyaeva <aleksandra.fedyaeva@gmail.com>
2025-08-27 01:33:28 +0400

    Добавила создание категорий в БД

--- FinanceTracker/AppDB.cs
+++ FinanceTracker/AppDB.cs
@@ -11,11 +11,40 @@ public class AppDB
     public void Initialize()
     {
         string dbPath = Android.App.Application.Context.GetDatabasePath("finance.db").AbsolutePath;
+        
         connection = new SQLiteConnection(dbPath);
+        
         connection.CreateTable<ExpenseCategory>();
+        
         connection.CreateTable<IncomeCategory>();
+        
         connection.CreateTable<ExpenseOperation>();
+        
         connection.CreateTable<IncomeOperation>();
+        
+        var existingExpenseCategory = connection.Table<ExpenseCategory>()
+            .FirstOrDefault(c => c.Name == "Продукты");
+        
+        if (existingExpenseCategory == null)
+        {
+            var newExpenseCategory = new ExpenseCategory
+            {
+                Name = "Продукты"
+            };
+            connection.Insert(newExpenseCategory);
+        }
+        
+        var existingIncomeCategory = connection.Table<IncomeCategory>()
+            .FirstOrDefault(c => c.Name == "Зарплата");
+        
+        if (existingIncomeCategory == null)
+        {
+            var newIncomeCategory = new IncomeCategory
+            {
+                Name = "Зарплата"
+            };
+            connection.Insert(newIncomeCategory);
+        }
     }
     
     public AppDB()
